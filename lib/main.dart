01f1184453f858f9a055b95876b0c3c2b14da861
Ahmed-Khalifa <ahmedkhalifa090@gmail.com>
2020-11-18 19:36:26 +0300

    Finished the Challenge.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.deepPurple[900],
         appBar: AppBar(
-          title: Text('ٌRoll The Dice'),
+          title: Text('Roll The Dice'),
           backgroundColor: Colors.deepPurple[900],
         ),
         body: DicePage(),
@@ -15,18 +16,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
-          Expanded(child:
-          Image.asset('images/dice1.png'),),
-          Expanded(child:
-          Image.asset('images/dice2.png'),),
+          Expanded(
+            child:
+            FlatButton(
+              onPressed: () {
+                _rollDice();
+              },
+              padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0, bottom: 16.0),
+              child:
+              Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ), //Left Button
+          Expanded(
+            child:
+            FlatButton(
+              padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0, bottom: 16.0),
+              onPressed: () {
+                _rollDice();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),//Right Button
         ],
       ),
     );
+  }
+  void _rollDice(){
+    setState(() {
+      this.rightDiceNumber = Random().nextInt(6) + 1;
+      this.leftDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
